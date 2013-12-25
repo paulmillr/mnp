0ddda7e1660c733c936f6965060f4c259bd4785e
Gosha Arinich <me@goshakkk.name>
2013-12-25 17:14:28 +0300

    fix currency conversion

--- tax-calculator.js
+++ tax-calculator.js
@@ -1,10 +1,10 @@
 var oexToken = '8556217a83d84985930d67d6cf934289';
 
-var toCurrency = function(value, fromCode, toCode) {
+var toCurrency = function(value, toCode, fromCode) {
   if (!value) return 0;
   return fx(value).from(fromCode).to(toCode);
 };
-var fromCurrency = function(value, toCode, fromCode) {
+var fromCurrency = function(value, fromCode, toCode) {
   if (!value) return 0;
   return fx(value).from(fromCode).to(toCode);
 };
